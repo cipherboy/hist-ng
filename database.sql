2d65f0c30abaa3f15e9cc8445c4ca3bb4e2ab76b
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-10-09 19:28:15 -0400

    Add sessions to database
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- database.sql
+++ database.sql
@@ -8,13 +8,20 @@ CREATE TABLE projects (
     name TEXT UNIQUE
 );
 
+CREATE TABLE sessions (
+    -- rowid
+    name TEXT UNIQUE
+);
+
 CREATE TABLE executions (
     command_id INTEGER,
     project_id INTEGER,
+    session_id INTEGER,
 
     pwd TEXT,
     exec_time DATETIME DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY(command_id) REFERENCES commands(ROWID),
     FOREIGN KEY(project_id) REFERENCES projects(ROWID)
+    FOREIGN KEY(session_id) REFERENCES sessions(ROWID)
 );
